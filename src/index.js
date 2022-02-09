0eb0a4fc77222a10847367b4c6eba0747c4543aa
wisatta <wisatta@gmail.com>
2022-02-09 13:56:58 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0)
+    return 'zero';
+switch (number) {
+    case 1 : return 'one'; break; 
+    case 2 : return 'two'; break;
+    case 3 : return 'three'; break;
+    case 4 : return 'four'; break;
+    case 5 : return 'five'; break;
+    case 6 : return 'six'; break;
+    case 7 : return 'seven'; break;
+    case 8 : return 'eight'; break;
+    case 9 : return 'nine'; break;
+    case 10 : return 'ten' ; break;
+    case 11 : return 'eleven' ; break; 
+    case 12 : return 'twelve' ; break;
+    case 13 : return 'thirteen' ; break;
+    case 14 : return 'fourteen' ; break;
+    case 15 : return 'fifteen' ; break;
+    case 16 : return 'sixteen' ; break;
+    case 17 : return 'seventeen' ; break;
+    case 18 : return 'eighteen' ; break;
+    case 19 : return 'nineteen' ; break;
+    case 20 : return 'twenty'; break;
+    case 30 : return 'thirty'; break;
+    case 40 : return 'forty'; break;
+    case 50 : return 'fifty'; break;
+    case 60 : return 'sixty'; break;
+    case 70 : return 'seventy'; break;
+    case 80 : return 'eighty'; break;
+    case 90 : return 'ninety'; break;
+    case 100 : return 'one hundred'; break;
+    }
 }
